28e88db018475a2c86b0d451b12d91ac1a0e980f
James Browne <jamesbrowne@Jamess-MacBook-Pro.local>
2015-07-29 00:08:20 +0100

    Just extracted a method.

--- src/ProductListPageParser.php
+++ src/ProductListPageParser.php
@@ -3,6 +3,7 @@
 namespace Unculture\Scraper;
 
 use \DOMXPath;
+use \DOMElement;
 use \stdClass;
 
 /**
@@ -36,7 +37,23 @@ class ProductListPageParser
         $products = [];
         $productFragments = $this->xpath->query('//div[@class="productInner"]');
 
-        foreach($productFragments as $product) {
+        foreach($productFragments as $productFragment) {
+            $products[] = $this->getProduct($productFragment);
+        }
+
+        return $products;
+    }
+
+    /**
+     * Uses the DOMXpath object passed in the constructor to extract a single
+     * product given a DOMElement with the data for that product.
+     * Products 
+     *
+     * @param DOMElement $product The DOMElement fragment of the document containing the product
+     * @return stdClass An object with title, link and unit_price properties
+     */
+    public function getProduct(DOMElement $product)
+    {
             $title = trim($this->xpath->query('.//h3/a/text()', $product)->item(0)->nodeValue);
             $link = $this->xpath->query('.//h3/a', $product)->item(0)->getAttribute("href");
             $unitPrice = (float) preg_replace('/[^\d.]/', "", trim($this->xpath->query('.//p[@class="pricePerUnit"]/text()', $product)->item(0)->nodeValue));
@@ -46,10 +63,7 @@ class ProductListPageParser
             $result->link = $link;
             $result->unit_price = $unitPrice;
 
-            $products[] = $result;
-        }
-
-        return $products;
-
+            return $result;
     }
 }
+
